b662cecfaa382fef48fe14009f67cb9e721ddaf0
T279 <technetium279@gmail.com>
2017-12-31 17:05:59 -0500

    Commit rearranging negotiables, creating learnCSS directory, etc

--- learnJS/FirstScript.js
+++ learnJS/FirstScript.js
@@ -2,3 +2,7 @@
 function myFunction() {
   document.getElementById('demo').innerHTML = "Even More Javascript!";
 }
+
+function firstFunction() {
+  document.getElementById('demo').innerHTML = "Hello Javascript!";
+}
